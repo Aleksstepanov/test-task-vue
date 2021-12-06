fd3b19ad65fb562475f97c3a11c23a83a700689f
Aleksey Stepanov <stepanovl87@gmail.com>
2021-12-06 11:21:46 +0300

    refactoring styles dashboard page

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,26 +1,63 @@
 <template>
 	<Spinner v-if="getLoading" />
 	<v-main v-else>
-		<h3>This is Dashboard page!</h3>
 		<v-container v-if="getUserProfile">
-			<h3>Hi, Friend!</h3>
-			<ul>
-				<li>email: {{ returnLoginUser }}</li>
-				<li>created: {{ returnCreatedAcc }}</li>
-				<li>updated: {{ returnUpdateAcc }}</li>
-			</ul>
-			{{ getTotalCount }}
+			<v-card outlined shaped class="pl-10">
+				<v-card-title>Hi, Friend!</v-card-title>
+				<v-list>
+					<v-list-item>
+						<v-list-item-icon>
+							<v-icon>mdi-email</v-icon>
+						</v-list-item-icon>
+						<v-list-item-title>email:</v-list-item-title>
+						<v-list-item-subtitle>{{ returnLoginUser }}</v-list-item-subtitle>
+					</v-list-item>
+					<v-list-item>
+						<v-list-item-icon>
+							<v-icon>mdi-account-plus</v-icon>
+						</v-list-item-icon>
+						<v-list-item-title>account created:</v-list-item-title>
+						<v-list-item-subtitle>{{ returnCreatedAcc }}</v-list-item-subtitle>
+					</v-list-item>
+					<v-list-item>
+						<v-list-item-icon>
+							<v-icon>mdi-account-check</v-icon>
+						</v-list-item-icon>
+						<v-list-item-title>account updated:</v-list-item-title>
+						<v-list-item-subtitle>{{ returnUpdateAcc }}</v-list-item-subtitle>
+					</v-list-item>
+				</v-list>
+				<v-list>
+					<v-list-item>
+						<v-list-item-icon>
+							<v-icon>mdi-human</v-icon>
+						</v-list-item-icon>
+						<v-list-item-title
+							>Number of members in your circle:</v-list-item-title
+						>
+						<v-list-item-subtitle
+							><v-card-text class="text-text-h2">{{
+								getTotalCount
+							}}</v-card-text></v-list-item-subtitle
+						>
+					</v-list-item>
+				</v-list>
+
+				<v-container>
+					<v-card-text
+						>Your members list.
+						<a @click="linkClickHanlder"> Click here!</a></v-card-text
+					>
+				</v-container>
+			</v-card>
 		</v-container>
-		<v-container>
-			Your members:
-			<a @click="linkClickHanlder">Members</a></v-container
-		>
 	</v-main>
 </template>
 
 <script>
 import { mapGetters, mapActions } from "vuex";
 import Spinner from "@/components/Spinner.vue";
+import dayjs from "dayjs";
 
 export default {
 	name: "Dashboard",
@@ -46,10 +83,14 @@ export default {
 			return this.getUserProfile.login || "";
 		},
 		returnCreatedAcc() {
-			return this.getUserProfile.createdAt || "";
+			return (
+				dayjs(this.getUserProfile.createdAt).format("DD-MM-YYYY HH:mm:ss") || ""
+			);
 		},
 		returnUpdateAcc() {
-			return this.getUserProfile.updatedAt || "";
+			return (
+				dayjs(this.getUserProfile.updatedAt).format("DD-MM-YYYY HH:mm:ss") || ""
+			);
 		},
 	},
 };
